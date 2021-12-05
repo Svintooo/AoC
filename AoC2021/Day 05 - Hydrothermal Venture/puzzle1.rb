541640f781e05b4c46cd1986c37048c0f05b5c79
Svintooo <github.com@svintoo.com>
2021-12-05 23:58:31 +0100

    AoC2021 day05: Puzzle 1, ...

--- AoC2021/Day 05 - Hydrothermal Venture/puzzle1.rb
+++ AoC2021/Day 05 - Hydrothermal Venture/puzzle1.rb
@@ -5,5 +5,17 @@ data     = IO.read(filename)
 
 
 ## PARSING
+vents_lines = data.split(/\n/)
+                  .collect{|vents_line|
+                    vents_line.strip
+                              .split(/ *-> */)
+                              .collect{|coordinate|
+                                coordinate.split(/,/)
+                                          .map{|xy| xy.to_i }
+                              }
+                  }
+pp vents_lines
+
+
 ## CALCULATION
 ## ANSWER
