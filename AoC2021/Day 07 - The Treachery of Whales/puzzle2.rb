d0515f143fd2f1dffdca45fea50c9df4a167d377
Hugo Ankarloo <hugo.ankarloo@hiq.se>
2021-12-07 10:35:30 +0100

    AoC2021 day07: Puzzle 2, solved
    Change-Id: Id2b4b6997a83a26106a5c064473ed95d21f0eee3

---
+++ AoC2021/Day 07 - The Treachery of Whales/puzzle2.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+data = ARGF.read
+
+
+## PARSING
+crab_subs = data.strip.split(',').map(&:to_i).sort
+#p crab_subs #DEBUG
+
+
+## CHECK
+#N/A
+
+
+## CALCULATE
+# Mean value
+position = crab_subs.map(&:to_f)
+                    .yield_self{|a| a.sum/a.count }
+                    .to_i
+#p position #DEBUG
+
+# One of the surrounding positions of the mean value seams
+# to be the most optimal one (according to my tests).
+# Though, I am not sure why this is.
+positions = [position-1, position, position+1]
+#p positions
+
+# Calculate the fuel cost of all positions
+fuels = positions.map{|pos|
+                   crab_subs.map{|v|
+                     # Calculate triangular number (4 -> 1+2+3+4)
+                     t = (v-pos).abs
+                     (t * (t + 1)) / 2
+                   }.sum
+                 }
+#p fuels
+
+fuel = fuels.min
+#p fuel #DEBUG
+
+
+## ANSWER
+answer = fuel
+puts answer
