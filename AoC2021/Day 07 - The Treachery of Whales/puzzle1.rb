c860e921c1f3e2138d13de2805d3ab55fc9ba654
Hugo Ankarloo <hugo.ankarloo@hiq.se>
2021-12-07 09:23:35 +0100

    AoC2021 day07: Puzzle 1, solved
    Change-Id: I86c2fb82bb06724e45216edc6346dc1451ad0def

---
+++ AoC2021/Day 07 - The Treachery of Whales/puzzle1.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+data = ARGF.read
+
+
+## PARSING
+crab_subs = data.strip.split(',').map(&:to_i).sort
+#p crab_subs #DEBUG
+
+
+## CHECK
+#N/A
+
+
+## CALCULATE
+position = crab_subs[(crab_subs.count/2).floor]  # Mean value
+#p position #DEBUG
+
+fuel = crab_subs.map{|v| (v-position).abs }.sum
+#p fuel #DEBUG
+
+
+## ANSWER
+answer = fuel
+puts answer
