8848fbf89183cebc2d9e6111553a48289bada2c3
Svintooo <github.com@svintoo.com>
2021-12-08 22:48:46 +0100

    AoC2021 day08: Puzzle 1 solved

--- AoC2021/Day 08 - Seven Segment Search/puzzle1.rb
+++ AoC2021/Day 08 - Seven Segment Search/puzzle1.rb
@@ -1,16 +1,34 @@
 #!/usr/bin/env ruby
+
+
+## INPUT
 data = ARGF.read
 
 
 ## PARSING
+signals = data.split(/\r\n?|\n/)
+              .map{|line|
+                line.split('|')
+                    .map{|numbers|
+                      numbers.split.map(&:strip)
+                    }
+              }
+              .map{|entry| [:patterns,:output].zip(entry).to_h }
+#pp signals  #DEBUG
 
 
 ## CHECK
+#N/A
 
 
 ## CALCULATE
+count_1_4_7_8 = 0
+
+signals.each do |entry|
+  entry[:output].each{|abcdefg| count_1_4_7_8 += 1 if [2,3,4,7].include?(abcdefg.length) }
+end
 
 
 ## ANSWER
-answer = nil
+answer = count_1_4_7_8
 puts answer
