884d26a78af92fc741777f7c326dc946ac4bcaff
Hugo Ankarloo <hugo.ankarloo@hiq.se>
2021-12-09 11:07:44 +0100

    AoC2021 day09: Puzzle 1 solved
    Change-Id: I94189d974ee845b5557a6d1218080333a877a2d4

--- AoC2021/Day 09 - Smoke Basin/puzzle1.rb
+++ AoC2021/Day 09 - Smoke Basin/puzzle1.rb
@@ -6,11 +6,31 @@ data = ARGF.read
 
 
 ## PARSING
+heightmap = data.split(/\r\n?|\n/)
+                .map{|line| line.chars.map{|c| c.to_i } }
+#pp heightmap #DEBUG
 
 
 ## CALCULATE
+low_points = []
+heightmap.each_with_index do |horizontal,y|
+  horizontal.each_with_index do |height, x|
+    is_lowpoint = true
+    is_lowpoint &= y == 0                    || height < heightmap[y-1][x]
+    is_lowpoint &= y == heightmap.count-1    || height < heightmap[y+1][x]
+    is_lowpoint &= x == 0                    || height < heightmap[y][x-1]
+    is_lowpoint &= x == heightmap[y].count-1 || height < heightmap[y][x+1]
+    low_points << [x,y] if is_lowpoint
+  end
+end
+#p low_points #DEBUG
+
+low_point_heights = low_points.map{|x,y| heightmap[y][x] }
+#p low_point_heights #DEBUG
+
+risk_levels = low_point_heights.map{|h| h+1 }
 
 
 ## ANSWER
-answer = nil
+answer = risk_levels.sum
 puts answer
