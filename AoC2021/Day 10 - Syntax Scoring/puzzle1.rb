4fceee1bb9a2bb4fea7ed07c51de1596c0f31468
Svintooo <github.com@svintoo.com>
2021-12-10 06:55:45 +0100

    AoC2021 day010: Puzzle 1, ...

--- AoC2021/Day 10 - Syntax Scoring/puzzle1.rb
+++ AoC2021/Day 10 - Syntax Scoring/puzzle1.rb
@@ -6,11 +6,40 @@ data = ARGF.read
 
 
 ## PARSING
+navigation_subsystem = data.split(/\r\n?|\n/)
+                           .map{|line| line.chars }
+
+
+
+## CHECK
+unknown_chars =
+  navigation_subsystem.all?{|line|
+                        line.all?{|char|
+                          ['(',')','[',']','{','}','<','>'].include?(char)
+                        }
+                      }
+raise "Input data error" unless unknown_chars
 
 
 ## CALCULATE
+score_points = {
+  ')' => 3,
+  ']' => 57,
+  '}' => 1197,
+  '>' => 25137,
+}
+
+total_score = 0
+
+open_chunks = []
+
+navigation_subsystem.each do |line|
+  line.each do |char|
+
+  end
+end
 
 
 ## ANSWER
-answer = nil
+answer = total_score
 puts answer
