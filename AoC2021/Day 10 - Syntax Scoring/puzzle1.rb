bdb2693b73e52fbcf6de4abe5b7951dc76031459
Svintooo <github.com@svintoo.com>
2021-12-10 07:03:17 +0100

    AoC2021 day010: Puzzle 1, ...

--- AoC2021/Day 10 - Syntax Scoring/puzzle1.rb
+++ AoC2021/Day 10 - Syntax Scoring/puzzle1.rb
@@ -29,15 +29,39 @@ score_points = {
   '>' => 25137,
 }
 
-total_score = 0
+chunk_pairs = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}',
+  '<' => '>',
+}
+
+incomplete_count = 0
+corrupted_count  = 0
+total_score      = 0
 
 open_chunks = []
 
 navigation_subsystem.each do |line|
   line.each do |char|
-
+    case char
+      when '(','[','{','<'
+        open_chunks << char
+      when ')',']','}','>'
+        if open_chunks.empty?
+          incomplete_count += 1
+        elsif open_chunks.last != chunk_pairs[char]
+          corrupted_count += 1
+          total_score += score_points[char]
+        else
+          open_chunks.pop
+        end
+    end
   end
 end
+p incomplete_count #DEBUG
+p corrupted_count #DEBUG
+p total_score #DEBUG
 
 
 ## ANSWER
