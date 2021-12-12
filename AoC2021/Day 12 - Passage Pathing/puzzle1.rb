6ff87bec01e1dee7274f4bb3d8d7d897331abcec
Svintooo <github.com@svintoo.com>
2021-12-12 10:21:36 +0100

    AoC2021 day12: Puzzle 1, ...

--- AoC2021/Day 12 - Passage Pathing/puzzle1.rb
+++ AoC2021/Day 12 - Passage Pathing/puzzle1.rb
@@ -3,6 +3,8 @@
 
 ## INPUT
 data = ARGF.read
+start_cave = "start"
+end_cave   = "end"
 
 
 ## PARSING
@@ -15,16 +17,48 @@ cave_paths = data.lines.map(&:strip)
                  }
 
 
+## HELP CODE
+class String
+  def upcase?
+    self.match? /\A\p{Upper}+\z/
+  end
+  def downcase?
+    self.match? /\A\p{Lower}+\z/
+  end
+end
+
+def travel(travel_log, small_caves_visited, next_cave)
+  [
+    travel_log << next_cave,
+    next_cave
+  ]
+end
+
+
+## CHECK
+# Check that all caves are not written with a mix of uppercase and lowercase characters
+raise "Input Error: Character Case" unless cave_paths.keys.all?{|cave| cave.upcase? || cave.downcase? }
+
+raise "Input Error: Start cave not found: #{start_cave}" unless cave_paths.has_key?(start_cave)
+raise "Input Error: End cave not found: #{end_cave}"     unless cave_paths.has_key?(end_cave)
+
+
 ## CALCULATE
 cave_full_paths = []
 queued_paths    = []
 
-initial_cave = "start"
-cave_paths[initial_cave].each{|cave|
-                          #Data explanation:  Travel        Small Caves   Next
-                          #                   Log           Visited       Cave
-                          queued_paths << [ [initial_cave], [],           cave ]
-                        }
+travel = ->() {
+  #
+}
+cave_paths[start_cave].each{|cave|
+                        #Data explanation:  Travel        Small Caves   Next
+                        #                   Log           Visited       Cave
+                        queued_paths << [ [start_cave], [],           cave ]
+                      }
+
+while (travel_log, small_caves_visited, cave = queued_paths.shift)
+  #
+end
 
 
 ## ANSWER
