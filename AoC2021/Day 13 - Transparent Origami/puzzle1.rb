6494b23ffaf44f8fd216bc897409413241030727
Svintooo <github.com@svintoo.com>
2021-12-13 07:35:14 +0100

    AoC2021 day13: Initial commit

---
+++ AoC2021/Day 13 - Transparent Origami/puzzle1.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+
+## INPUT
+data = ARGF.read
+
+
+## PARSING
+asdf = data.lines.map(&:strip)
+
+
+## HELP CODE
+
+
+## CHECK
+
+
+## CALCULATE
+
+
+## ANSWER
+answer = nil
+puts answer
