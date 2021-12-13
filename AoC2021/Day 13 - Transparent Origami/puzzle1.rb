999c7c81c58ea2895cc5bcea3536a1c54d3e7a6c
Svintooo <github.com@svintoo.com>
2021-12-13 08:08:45 +0100

    AoC2021 day13: Puzzle 1, ...

--- AoC2021/Day 13 - Transparent Origami/puzzle1.rb
+++ AoC2021/Day 13 - Transparent Origami/puzzle1.rb
@@ -6,9 +6,9 @@ dots = []
 fold = []
 
 ARGF.each_line do |line|
-  line = line.strip
+  line = line.strip  # Remove newline character
 
-  if [0123456789].include? line.chars[0]
+  if [0,1,2,3,4,5,6,7,8,9].include? line.chars[0]
     dots << line
   elsif line == ""
     next
@@ -21,8 +21,9 @@ end
 
 ## PARSING
 dots.map!{|dot| dot.split(',').map(&:to_i) }
-fold_x
-fold_y
+
+fold_x = nil
+fold_y = nil
 fold.each{|line|
            key,val = line.split.last.split('=')
            fold_x = val.to_i if key == 'x'
@@ -31,6 +32,21 @@ fold.each{|line|
 
 
 ## HELP CODE
+# Create the paper
+paper = []
+paper_height = dots.map{|_,y| y }.max
+paper_width  = dots.map{|x,_| x }.max
+
+paper_height.each do |i|
+  paper[i] == Array.new(paper_width, '.')
+end
+pp paper #DEBUG
+
+# Fill in dots on paper
+dots.each do |x,y|
+  paper[y][x] = '#'
+end
+pp paper #DEBUG
 
 
 ## CHECK
