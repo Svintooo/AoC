ebdedcbe6cd63a928e46119ec63b718642528b28
Svintooo <github.com@svintoo.com>
2021-12-13 07:56:32 +0100

    AoC2021 day13: Puzzle 1, ...

--- AoC2021/Day 13 - Transparent Origami/puzzle1.rb
+++ AoC2021/Day 13 - Transparent Origami/puzzle1.rb
@@ -2,11 +2,32 @@
 
 
 ## INPUT
-data = ARGF.read
+dots = []
+fold = []
+
+ARGF.each_line do |line|
+  line = line.strip
+
+  if [0123456789].include? line.chars[0]
+    dots << line
+  elsif line == ""
+    next
+  else#if line.split[0] == "fold"
+    fold << line
+  end
+end
+
 
 
 ## PARSING
-asdf = data.lines.map(&:strip)
+dots.map!{|dot| dot.split(',').map(&:to_i) }
+fold_x
+fold_y
+fold.each{|line|
+           key,val = line.split.last.split('=')
+           fold_x = val.to_i if key == 'x'
+           fold_y = val.to_i if key == 'y'
+         }
 
 
 ## HELP CODE
