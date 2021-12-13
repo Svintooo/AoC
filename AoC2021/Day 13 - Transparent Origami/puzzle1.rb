1c0112a41d3fa268457459060d114a2b449dde45
Svintooo <github.com@svintoo.com>
2021-12-13 08:17:26 +0100

    AoC2021 day13: Puzzle 1, ...

--- AoC2021/Day 13 - Transparent Origami/puzzle1.rb
+++ AoC2021/Day 13 - Transparent Origami/puzzle1.rb
@@ -2,20 +2,22 @@
 
 
 ## INPUT
-dots = []
-fold = []
+dots  = []
+folds = []
 
 ARGF.each_line do |line|
   line = line.strip  # Remove newline character
 
-  if [0,1,2,3,4,5,6,7,8,9].include? line.chars[0]
+  if ["0","1","2","3","4","5","6","7","8","9"].include? line.chars[0]
     dots << line
   elsif line == ""
     next
   else#if line.split[0] == "fold"
-    fold << line
+    folds << line
   end
 end
+#p dots #DEBUG
+#p folds #DEBUG
 
 
 
@@ -24,29 +26,36 @@ dots.map!{|dot| dot.split(',').map(&:to_i) }
 
 fold_x = nil
 fold_y = nil
-fold.each{|line|
-           key,val = line.split.last.split('=')
-           fold_x = val.to_i if key == 'x'
-           fold_y = val.to_i if key == 'y'
-         }
+folds.each{|line|
+            key,val = line.split.last.split('=')
+            fold_x = val.to_i if key == 'x'
+            fold_y = val.to_i if key == 'y'
+          }
+#p dots #DEBUG
+#p [fold_x,fold_y] #DEBUG
 
 
 ## HELP CODE
+
 # Create the paper
 paper = []
-paper_height = dots.map{|_,y| y }.max
-paper_width  = dots.map{|x,_| x }.max
+paper_height = dots.map{|_,y| y }.max + 1
+paper_width  = dots.map{|x,_| x }.max + 1
+#p paper_height #DEBUG
+#p paper_width #DEBUG
 
-paper_height.each do |i|
-  paper[i] == Array.new(paper_width, '.')
+paper_height.times do |i|
+  paper[i] = Array.new(paper_width, '.')
 end
-pp paper #DEBUG
+#pp paper #DEBUG
+
 
 # Fill in dots on paper
 dots.each do |x,y|
   paper[y][x] = '#'
 end
-pp paper #DEBUG
+#pp paper #DEBUG
+#paper.each{|line| puts line.join } #DEBUG
 
 
 ## CHECK
