0f0e5542960b6d2b017a410873874c3d23767951
Svintooo <github.com@svintoo.com>
2021-12-14 08:36:35 +0100

    AoC2021 day14: Puzzle 2, solved

--- AoC2021/Day 14 - Extended Polymerization/puzzle2.rb
+++ AoC2021/Day 14 - Extended Polymerization/puzzle2.rb
@@ -40,49 +40,47 @@ polymer_pairs = {}
   polymer_pairs[pair] ||= 0
   polymer_pairs[pair] += 1
 end
-pp polymer_pairs #DEBUG
+#pp polymer_pairs #DEBUG
 
-#40.times do |step|
-10.times do |step| #DEBUG
-  debug = step+1 == 3 #DEBUG
-  polymer_pairs.keys.each do |pair|
+element_count = {}
+template.each{|element|
+               element_count[element] ||= 0
+               element_count[element] += 1
+             }
+#pp element_count #DEBUG
+
+40.times do |step|
+  #debug = false#step+1 == 1 #DEBUG
+  tmp_polymer_pairs = {}
+
+  polymer_pairs.each do |pair, count|
     insertion = rules[pair]
+    element_count[insertion.first] ||= 0
+    element_count[insertion.first] += count
+
     new_pair1 = pair[0..0] + insertion
     new_pair2 = insertion + pair[1..1]
-    puts if debug #DEBUG
-    puts "#{pair.join} => #{new_pair1.join} #{new_pair2.join}" if debug #DEBUG
+    #puts if debug #DEBUG
+    #puts "#{pair.join} => #{new_pair1.join} #{new_pair2.join}" if debug #DEBUG
 
-    polymer_pairs[pair] -= 1
-    polymer_pairs.delete(pair) if polymer_pairs[pair] <= 0
+    tmp_polymer_pairs[new_pair1] ||= 0
+    tmp_polymer_pairs[new_pair2] ||= 0
 
-    polymer_pairs[new_pair1] ||= 0
-    polymer_pairs[new_pair2] ||= 0
+    tmp_polymer_pairs[new_pair1] += count
+    tmp_polymer_pairs[new_pair2] += count
 
-    polymer_pairs[new_pair1] += 1
-    polymer_pairs[new_pair2] += 1
-    pp polymer_pairs if debug #DEBUG
+    #pp polymer_pairs if debug #DEBUG
   end
 
-  puts #DEBUG
-  puts "Step #{step+1}" #DEBUG
-  pp polymer_pairs #DEBUG
-  exit if debug #DEBUG
+  polymer_pairs = tmp_polymer_pairs
 end
 
 #p polymer #DEBUG
-element_count = polymer_pairs.inject({}){|hashmap,(pair,count)|
-                               hashmap[pair.first] ||= 0
-                               hashmap[pair.last ] ||= 0
-
-                               hashmap[pair.first] += count
-                               hashmap[pair.last ] += count
-
-                               hashmap
-                             }
 
 most_common_element  = element_count.max{|(char1,count1),(char2,count2)| count1 <=> count2 }.first
 least_common_element = element_count.min{|(char1,count1),(char2,count2)| count1 <=> count2 }.first
-
+#p [  most_common_element ,element_count[most_common_element ]  ] #DEBUG
+#p [  least_common_element,element_count[least_common_element]  ] #DEBUG
 
 ## ANSWER
 answer = element_count[most_common_element] - element_count[least_common_element]
