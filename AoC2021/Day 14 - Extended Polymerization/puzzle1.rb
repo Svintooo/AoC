13250b3689b6fdd91b21f1394e0490ea92ed102a
Svintooo <github.com@svintoo.com>
2021-12-14 07:23:31 +0100

    AoC2021 day14: Puzzle 1, solved

--- AoC2021/Day 14 - Extended Polymerization/puzzle1.rb
+++ AoC2021/Day 14 - Extended Polymerization/puzzle1.rb
@@ -2,22 +2,69 @@
 
 
 ## INPUT
-data = ARGF.read
+template = nil
+rules    = []
+
+ARGF.each_line do |line|
+  line = line.strip  # Remove newline character
+
+  if line.match? /->/
+    rules << line
+  elsif line == ""
+    next
+  else
+    template = line
+  end
+end
+#p template #DEBUG
+#p rules #DEBUG
 
 
 ## PARSING
-asdf = data.lines.map(&:strip)
-
-
-## HELP CODE
-
-
-## CHECK
+template = template.chars
+rules = rules.map{|rule|
+               rule.split(/\s*->\s*/)
+                   .map{|str| str.chars }
+             }
+             .to_h
+#p template #DEBUG
+#pp rules #DEBUG
 
 
 ## CALCULATE
+polymer = template.clone
+
+#puts "Template: #{template.join}" #DEBUG
+
+10.times do |step|
+  i = 0
+
+  while i <= polymer.length-2
+    key = polymer[(i)..(i+1)]
+
+    if rules.has_key?(key)
+      insertion = rules[key]
+      polymer = polymer[0..i] + insertion + polymer[(i+1)..-1]
+      i += 1
+    end
+
+    i += 1
+  end
+
+  #puts "After step #{step+1}: #{polymer.join}" #DEBUG
+end
+
+#p polymer #DEBUG
+element_count = polymer.inject({}){|hashmap, char|
+                                    hashmap[char] ||= 0
+                                    hashmap[char] += 1
+                                    hashmap
+                                  }
+
+most_common_element  = element_count.max{|(char1,count1),(char2,count2)| count1 <=> count2 }.first
+least_common_element = element_count.min{|(char1,count1),(char2,count2)| count1 <=> count2 }.first
 
 
 ## ANSWER
-answer = nil
+answer = element_count[most_common_element] - element_count[least_common_element]
 puts answer
