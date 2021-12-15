09cbb7cf9508758271db72332992dc1c389015e1
Svintooo <github.com@svintoo.com>
2021-12-15 06:38:47 +0100

    AoC2021 day15: Puzzle 1, ...

--- AoC2021/Day 15 - Chiton/puzzle1.rb
+++ AoC2021/Day 15 - Chiton/puzzle1.rb
@@ -5,17 +5,41 @@
 data = ARGF.read
 
 
+## CHECK
+raise "Input contains non-integer characters" if data.match? /[^0-9]/
+
+
 ## PARSING
-asdf = data.lines.map(&:strip)
+map = data.lines.map(&:strip)
+                .map{|line| line.chars.map(&:to_i) }
 
 
 ## HELP CODE
+# A crude priority queue
+path_queue = {}
+def path_queue.<<(path_queue_data)
+  xy,history,prio = path_queue_data
+  self[:current_prio] ||= 0
+  self[:current_prio] = [ prio, self[:current_prio] ].min
 
+  self[prio] ||= []
+  self[prio] << [xy,history]
+end
+def path_queue.pop
+  prio = self[:current_prio]
+  element = self[prio].shift
 
-## CHECK
+  if self[prio].empty?
+    self.delete(prio)
+    self[:current_prio] = self.keys.reject{|k|k=:current_prio}.min
+  end
+
+  return element
+end
 
 
 ## CALCULATE
+path_queue << [[0,0],[],1]  # initialize
 
 
 ## ANSWER
