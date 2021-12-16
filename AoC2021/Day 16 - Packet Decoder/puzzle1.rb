0997744b9a4dc51fec878e075dd5efcb29d16e84
Svintooo <github.com@svintoo.com>
2021-12-16 06:44:54 +0100

    AoC2021 day16: Puzzle 1, ...

--- AoC2021/Day 16 - Packet Decoder/puzzle1.rb
+++ AoC2021/Day 16 - Packet Decoder/puzzle1.rb
@@ -16,12 +16,14 @@ hexadecimal = data.strip
 
 
 ## CALCULATE
-binary = hexadecimal.to_i(16).to_s(2).chars.map{|bin| bin.to_i }
+#binary = hexadecimal.to_i(16).to_s(2).chars.map(&:to_i)
+binary = hexadecimal.chars.map{|hex| hex.to_i(16).to_s(2).rjust(4,'0') }.join.chars#.map(&:to_i)
 p binary.join #DEBUG
-package = {}
-package[:version] = binary.shift(3).join
-package[:type_id] = binary.shift(3)
 
+package = {}
+package[:version] = binary.shift(3).join.to_i(2)
+package[:type_id] = binary.shift(3).join.to_i(2)
+pp package #DEBUG
 
 
 ## ANSWER
