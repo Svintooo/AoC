ccc513b46a77d22bd34197e5497c15305a2cb582
Svintooo <github.com@svintoo.com>
2021-12-18 18:17:07 +0100

    AoC2021 day17: Puzzle 1, ...

--- AoC2021/Day 17 - Trick Shot/puzzle1.rb
+++ AoC2021/Day 17 - Trick Shot/puzzle1.rb
@@ -27,7 +27,7 @@ def x_pos(t, v)
 
   x = (t <= v)  \
       ? v*t - (t**2 - t)/2  \
-      : (v**2 - v)/2
+      : v*v - (v**2 - v)/2
 
   x = -x if negate
   return x
@@ -53,18 +53,31 @@ x_enumerator = if target[:x].all?{|x| x >= 0}
                end
 
 x_enumerator.each do |x_velocity|
+  #print"x" #DEBUG
+  x = x_pos(x_velocity,x_velocity)
+  (puts".x1";next) if x > 0 && x < target[:x].min
+  (puts".x2";next) if x < 0 && x > target[:x].max
+  (puts".x3";next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
   (0..).each do |y_velocity|
+    #print"y" #DEBUG
+    (puts".x.y1";next) if [x_velocity,y_velocity] == [0,0]
     x = x_pos(y_velocity, x_velocity)
-    break if x_velocity >= 0 && x >= target[:x].max
-    break if x_velocity <  0 && x >= target[:x].min
-    (0..).each do |step|
+    puts;p([x_velocity,y_velocity,x]) #DEBUG
+    (puts".x.y2";break) if x > 0 && x >= target[:x].max
+    (puts".x.y3";break) if x < 0 && x <= target[:x].min
+    (puts".x.y4";next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
+    #print"." #DEBUG
+    #y = y_pos(1, y_velocity)
+    #break if y < target[:y].min
+    (1..).each do |step|
       x = x_pos(step, x_velocity)
       y = y_pos(step, y_velocity)
-      break if x_velocity >= 0 && x > target[:x].max
-      break if x_velocity <  0 && x < target[:x].min
-      break if                    y > target[:y].min
-      next unless (target[:x].min..target[:x].max).include?(x)
-      next unless (target[:y].min..target[:y].max).include?(y)
+      (puts".x.y.s1 #{[x_velocity,y_velocity,step,x,y]}";break) if x > 0 && x > target[:x].max
+      (puts".x.y.s2 #{[x_velocity,y_velocity,step,x,y]}";break) if x < 0 && x < target[:x].min
+      (puts".x.y.s3 #{[x_velocity,y_velocity,step,x,y]}";break) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
+      (puts".x.y.s4 #{[x_velocity,y_velocity,step,x,y]}";break) if y < target[:y].min
+      (puts".x.y.s5 #{[x_velocity,y_velocity,step,x,y]}";next) unless (target[:x].min..target[:x].max).include?(x)
+      (puts".x.y.s6 #{[x_velocity,y_velocity,step,x,y]}";next) unless (target[:y].min..target[:y].max).include?(y)
       solutions << [step, x_velocity, y_velocity]
     end
   end
@@ -72,10 +85,10 @@ end
 
 
 pp solutions #DEBUG
-p [x_pos(4,9), y_pos(4,0)] #DEBUG
+#p [x_pos(4,9), y_pos(4,0)] #DEBUG
 
 
 ## ANSWER
 answer = solutions.map{|_,_,y_velocity| y_velocity }.max
-              .yield_self{|y_velocity| y_pos(y_velocity, y_velocity) }
+                  .yield_self{|y_velocity| y_pos(y_velocity, y_velocity) }
 puts answer
