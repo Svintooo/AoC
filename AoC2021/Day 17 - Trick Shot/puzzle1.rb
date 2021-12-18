10c9f97a1b7182271f34fa6ea44273ea3ed93cda
Svintooo <github.com@svintoo.com>
2021-12-18 17:24:10 +0100

    AoC2021 day17: Puzzle 1, ...

--- AoC2021/Day 17 - Trick Shot/puzzle1.rb
+++ AoC2021/Day 17 - Trick Shot/puzzle1.rb
@@ -52,19 +52,20 @@ x_enumerator = if target[:x].all?{|x| x >= 0}
                  target[:x].min..target[:x].max
                end
 
-catch :STOP do
-  x_enumerator.each do |x_velocity|
-    (0..).each do |y_velocity|
-      x = x_pos(y_velocity, x_velocity)
-      break if x_velocity >= 0 && x >= target[:x].max
-      break if x_velocity <  0 && x >= target[:x].min
-      (0..).each do |step|
-        x = x_pos(step, x_velocity)
-        y = y_pos(step, y_velocity)
-        break if x_velocity >= 0 && x > target[:x].max
-        break if x_velocity <  0 && x < target[:x].min
-        break if                    y > target[:y].min
-      end
+x_enumerator.each do |x_velocity|
+  (0..).each do |y_velocity|
+    x = x_pos(y_velocity, x_velocity)
+    break if x_velocity >= 0 && x >= target[:x].max
+    break if x_velocity <  0 && x >= target[:x].min
+    (0..).each do |step|
+      x = x_pos(step, x_velocity)
+      y = y_pos(step, y_velocity)
+      break if x_velocity >= 0 && x > target[:x].max
+      break if x_velocity <  0 && x < target[:x].min
+      break if                    y > target[:y].min
+      next unless (target[:x].min..target[:x].max).include?(x)
+      next unless (target[:y].min..target[:y].max).include?(y)
+      solutions << [step, x_velocity, y_velocity]
     end
   end
 end
