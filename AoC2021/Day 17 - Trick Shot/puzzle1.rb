b05e60a7f1b01f18546145dde796043b4a13555a
Svintooo <github.com@svintoo.com>
2021-12-18 19:08:37 +0100

    AoC2021 day17: Puzzle 1, ...

--- AoC2021/Day 17 - Trick Shot/puzzle1.rb
+++ AoC2021/Day 17 - Trick Shot/puzzle1.rb
@@ -17,7 +17,7 @@ target = data.gsub("target area: ","")
                                 .sort
              }
              .yield_self{|d| [:x,:y].zip(d).to_h }
-p target #DEBUG
+p target; puts #DEBUG
 
 
 ## HELP CODE
@@ -48,6 +48,8 @@ end
 ## CALCULATE
 solutions = []
 
+DEBUG = false
+
 x_enumerator = if target[:x].all?{|x| x >= 0}
                  0..target[:x].max
                elsif target[:x].all?{|x| x < 0}
@@ -57,39 +59,37 @@ x_enumerator = if target[:x].all?{|x| x >= 0}
                end
 
 x_enumerator.each do |x_velocity|
-  #print"x" #DEBUG
   x = x_pos(x_velocity,x_velocity)
-  (puts".x1";next) if x > 0 && x < target[:x].min
-  (puts".x2";next) if x < 0 && x > target[:x].max
-  (puts".x3";next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
+  (puts".x1" if DEBUG;next) if x > 0 && x < target[:x].min
+  (puts".x2" if DEBUG;next) if x < 0 && x > target[:x].max
+  (puts".x3" if DEBUG;next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
   (0..).each do |y_velocity|
-    #print"y" #DEBUG
-    (puts".x.y1";next) if [x_velocity,y_velocity] == [0,0]
+    (puts".x.y1" if DEBUG;next) if [x_velocity,y_velocity] == [0,0]
     x = x_pos(y_velocity, x_velocity)
-    puts;p([x_velocity,y_velocity,x]) #DEBUG
-    (puts".x.y2";break) if x > 0 && x >= target[:x].max
-    (puts".x.y3";break) if x < 0 && x <= target[:x].min
-    (puts".x.y4";next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
-    #print"." #DEBUG
+    #puts;p([x_velocity,y_velocity,x]) #DEBUG
+    (puts".x.y2" if DEBUG;break) if x > 0 && x >= target[:x].max
+    (puts".x.y3" if DEBUG;break) if x < 0 && x <= target[:x].min
+    (puts".x.y4" if DEBUG;next) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
     start_step = step_where_y_is_zero(y_velocity) + 1
     break if y_pos(start_step, y_velocity) < target[:y].min
     (start_step..).each do |step|
       x = x_pos(step, x_velocity)
       y = y_pos(step, y_velocity)
-      (puts".x.y.s1 #{[x_velocity,y_velocity,step,x,y]}";break) if x > 0 && x > target[:x].max
-      (puts".x.y.s2 #{[x_velocity,y_velocity,step,x,y]}";break) if x < 0 && x < target[:x].min
-      (puts".x.y.s3 #{[x_velocity,y_velocity,step,x,y]}";break) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
-      (puts".x.y.s4 #{[x_velocity,y_velocity,step,x,y]}";break) if y < target[:y].min
-      (puts".x.y.s5 #{[x_velocity,y_velocity,step,x,y]}";next) unless (target[:x].min..target[:x].max).include?(x)
-      (puts".x.y.s6 #{[x_velocity,y_velocity,step,x,y]}";next) unless (target[:y].min..target[:y].max).include?(y)
+      (puts".x.y.s1 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;break) if x > 0 && x > target[:x].max
+      (puts".x.y.s2 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;break) if x < 0 && x < target[:x].min
+      (puts".x.y.s3 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;break) if x == 0 && !(target[:x].min..target[:x].max).include?(x)
+      (puts".x.y.s4 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;break) if y < target[:y].min
+      (puts".x.y.s5 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;next) unless (target[:x].min..target[:x].max).include?(x)
+      (puts".x.y.s6 #{[x_velocity,y_velocity,step,x,y]}" if DEBUG;next) unless (target[:y].min..target[:y].max).include?(y)
       solutions << [step, x_velocity, y_velocity]
     end
   end
 end
 
-
-pp solutions #DEBUG
-#p [x_pos(4,9), y_pos(4,0)] #DEBUG
+#DEBUG
+pp solutions
+#p [x_pos(4,9), y_pos(4,0)]
+puts
 
 
 ## ANSWER
