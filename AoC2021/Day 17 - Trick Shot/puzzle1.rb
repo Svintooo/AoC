014fddef54775f3106c9aba9b9855e8e6d62f3df
Svintooo <github.com@svintoo.com>
2021-12-18 15:43:30 +0100

    AoC2021 day17: Puzzle 1, ...

--- AoC2021/Day 17 - Trick Shot/puzzle1.rb
+++ AoC2021/Day 17 - Trick Shot/puzzle1.rb
@@ -45,18 +45,21 @@ end
 
 #all [V,t] where t=V..1 and V=(Tx.last)..1 and x(t)=(Tx.last)..(Tx.first)
 x_v_t = []
+#TODO: support for negative values in target[:x]
 1.upto(target[:x].last).each do |velocity| #V=(Tx.last)..1
   1.upto(velocity).each do |step| #t=V..1
-    x = x_pos(velocity,step)
-    x_v_t << [velocity,step] if x >= target[:x].first && x <= target[:x].last
+    x = x_pos(velocity, step)
+    x_v_t << [velocity, step] if x >= target[:x].first && x <= target[:x].last
   end
 end
 pp x_v_t #DEBUG
 
 #all [A,t] where t in Vts and A=1..t and y(t)=(Ty.last)..(Ty.first)
+y_v_t = []
 x_v_t.each do |_,step| #t in Vts
   1.upto(step) do |velocity| #A=1..t
-    #
+    y = y_pos(velocity, step)
+    y_v_t << [velocity, step] if y >= target[:y].first && y <= target[:y].last
   end
 end
 
