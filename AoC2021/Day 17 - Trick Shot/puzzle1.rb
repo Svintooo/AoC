754ee4dc55908f81acb945b93e08e5c9ca935dc6
Svintooo <github.com@svintoo.com>
2021-12-18 15:35:21 +0100

    AoC2021 day17: Puzzle 1, ...

--- AoC2021/Day 17 - Trick Shot/puzzle1.rb
+++ AoC2021/Day 17 - Trick Shot/puzzle1.rb
@@ -21,44 +21,6 @@ p target #DEBUG
 
 
 ## HELP CODE
-class Position
-  def initialize(initial_velosity, type)
-    @initial_velosity = initial_velosity
-    @v = initial_velosity
-    @value = 0
-
-    @type = type
-  end
-
-  def position(t)
-    case @type
-      when :x then pos_x(t)
-      when :y then pos_y(t)
-    end
-  end
-  alias :pos :position
-
-  def pos_x(t)
-    v = @initial_velosity
-    negate = v < 0
-    v = -v if negate
-
-    x = (t <= v) ? ( v*t - (t**2 - t)/2 ) \
-                 : ( (v**2 - v)/2 )
-
-    x = -x if negate
-    return x
-  end
-
-  def pos_y(t)
-    v = @initial_velosity
-    v*t - (t**2 - t)/2
-  end
-end
-#p Position.new( 5,:y).position(3)  #  12
-#p Position.new( 5,:x).position(3)  #  12
-#p Position.new(-5,:x).position(3)  # -12
-
 def x_pos(v, t)
   negate = v < 0
   v = -v if negate
