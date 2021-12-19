b563c135e99e8cb950b606352157b3a6939ff4fe
Svintooo <github.com@svintoo.com>
2021-12-19 09:37:15 +0100

    AoC2021 day18: Puzzle 1, ...

--- AoC2021/Day 18 - Snailfish/puzzle1.rb
+++ AoC2021/Day 18 - Snailfish/puzzle1.rb
@@ -8,6 +8,10 @@ data = case when File.exists?(ARGV[0])
             end
 
 
+## CHECK
+raise "Invalid input data" unless data.match? /\A[\[\],0-9\s]+\z/
+
+
 ## PARSING
 numbers_list = data.lines.map(&:strip)
                    .map{|line| JSON.parse(line) }
@@ -22,18 +26,20 @@ def split(number)
 end
 
 
-## CHECK
-
-
 ## CALCULATE
 numbers_list.each do |numbers|
   integer_indexes = []
   queue = [ [0], [1] ]
-  while indexes = queue.unshift do
-    case numbers.dig(*indexes)
+  while indexes = queue.shift do
+    #p indexes #DEBUG
+    case o = numbers.dig(*indexes)
       when Array
+        queue.unshift indexes+[1]
+        queue.unshift indexes+[0]
       when Integer
+        integer_indexes << indexes
       else
+        $stderr.puts "WARN: Unknown data `#{o.inspect}`"
       #end
     end
   end
