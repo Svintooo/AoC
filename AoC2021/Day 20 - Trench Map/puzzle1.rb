cde3d1fdbf2634a347bc564d600a17cf9ed5a8dc
Svintooo <github.com@svintoo.com>
2021-12-22 15:12:50 +0100

    AoC2021 day20: Puzzle 1, thx random redditer
    I knew I didn't understand wy the puzzle repeatedly mentioned
    that the picture is infinite, but I couldn't figure it out.
    
    Thank you random redditer: https://www.reddit.com/r/adventofcode/comments/rm43rr/2021_20_part1python_anyone_finds_the_mistake/hpjrz9o/
    
    It is clear to me know that I must apply the algorithm on the whole
    infinite picture, not just the converted picture data.
    
    Though I'm still not getting the right answer. Hmm...
    
    Change-Id: I83896a2bed1534475cc8c048c8e9133dcc78cc32

--- AoC2021/Day 20 - Trench Map/puzzle1.rb
+++ AoC2021/Day 20 - Trench Map/puzzle1.rb
@@ -14,14 +14,13 @@ algorithm, input_image = data.split(/#{NEWLINE}#{NEWLINE}/)
 
 algorithm = algorithm.strip.chars
 
-DEFAULT = algorithm[0]
-
 input_image = input_image.lines.map{|line| line.strip.chars }
 input_image.each{|line| puts line.join };puts #DEBUG
 #exit #DEBUG EXIT
 
 
 ## CHECK
+raise "Algorithm is of wrong length" unless algorithm.length == 512
 #exit #DEBUG EXIT
 
 
@@ -31,37 +30,40 @@ input_image.each{|line| puts line.join };puts #DEBUG
 
 ## CALCULATE
 output_image = input_image
+filler_char = algorithm[0]
 
 2.times do |step|
-  #puts"### #{step} ###"
+  puts"### #{step} ###"
 
-  work_image = output_image.map{|line| [DEFAULT]+line+[DEFAULT] }
-  work_image.unshift Array.new(work_image[0].length, DEFAULT)
-  work_image.push    Array.new(work_image[0].length, DEFAULT)
-  #work_image.each{|line| puts line.join };puts #DEBUG
+  work_image = output_image.map{|line| [filler_char]+line+[filler_char] }
+  work_image.unshift Array.new(work_image[0].length, filler_char)
+  work_image.push    Array.new(work_image[0].length, filler_char)
+  work_image.each{|line| puts line.join };puts #DEBUG
 
-  output_image = work_image.map{|line| Array.new(line.length, DEFAULT) }
-  #output_image.each{|line| puts line.join };puts #DEBUG
+  output_image = work_image.map{|line| Array.new(line.length, filler_char) }
+  output_image.each{|line| puts line.join };puts #DEBUG
 
   m=work_image.length-1
   work_image.each_with_index do |line, i|
     w=line.length-1
     line.each_index do |j|
       index = [[],[],[]]
-      index[0][0] = (i==0||j==0) ? (DEFAULT) : (work_image[i-1][j-1])
-      index[0][1] = (i==0      ) ? (DEFAULT) : (work_image[i-1][j  ])
-      index[0][2] = (i==0||j==w) ? (DEFAULT) : (work_image[i-1][j+1])
-      index[1][0] = (      j==0) ? (DEFAULT) : (work_image[i  ][j-1])
-      index[1][1] =                        (work_image[i  ][j  ])
-      index[1][2] = (      j==w) ? (DEFAULT) : (work_image[i  ][j+1])
-      index[2][0] = (i==m||j==0) ? (DEFAULT) : (work_image[i+1][j-1])
-      index[2][1] = (i==m      ) ? (DEFAULT) : (work_image[i+1][j  ])
-      index[2][2] = (i==m||j==w) ? (DEFAULT) : (work_image[i+1][j+1])
+      index[0][0] = (i==0||j==0) ? (filler_char) : (work_image[i-1][j-1])
+      index[0][1] = (i==0      ) ? (filler_char) : (work_image[i-1][j  ])
+      index[0][2] = (i==0||j==w) ? (filler_char) : (work_image[i-1][j+1])
+      index[1][0] = (      j==0) ? (filler_char) : (work_image[i  ][j-1])
+      index[1][1] =                                (work_image[i  ][j  ])
+      index[1][2] = (      j==w) ? (filler_char) : (work_image[i  ][j+1])
+      index[2][0] = (i==m||j==0) ? (filler_char) : (work_image[i+1][j-1])
+      index[2][1] = (i==m      ) ? (filler_char) : (work_image[i+1][j  ])
+      index[2][2] = (i==m||j==w) ? (filler_char) : (work_image[i+1][j+1])
 
-      index = index.flatten.map{|char| char==DEFAULT ? 0 : 1 }.join.to_i(2)
+      index = index.flatten.map{|char| char==filler_char ? 0 : 1 }.join.to_i(2)
       output_image[i][j] = algorithm[index]
     end
   end
+
+  filler_char = (filler_char==".") ? algorithm[0] : algorithm[-1]
 
   output_image.each{|line| puts line.join };puts #DEBUG
 end
