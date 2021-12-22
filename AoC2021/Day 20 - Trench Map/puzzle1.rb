3d41f6625d3cc3c5c97329f49e59360f078a9964
Svintooo <github.com@svintoo.com>
2021-12-22 16:46:09 +0100

    AoC2021 day20: Puzzle 1, solved
    Change-Id: I1cd8cbe60177aa478b54aa78c9a11ab9f8354eda

--- AoC2021/Day 20 - Trench Map/puzzle1.rb
+++ AoC2021/Day 20 - Trench Map/puzzle1.rb
@@ -13,9 +13,13 @@ algorithm, input_image = data.split(/#{NEWLINE}#{NEWLINE}/)
 #exit #DEBUG EXIT
 
 algorithm = algorithm.strip.chars
+#algorithm[0]='#';algorithm[-1]='.';#DEBUG DELETEME
+#puts algorithm.join;puts #DEBUG
+#exit #DEBUG EXIT
+
 
 input_image = input_image.lines.map{|line| line.strip.chars }
-input_image.each{|line| puts line.join };puts #DEBUG
+#input_image.each{|line| puts line.join };puts #DEBUG
 #exit #DEBUG EXIT
 
 
@@ -29,43 +33,54 @@ raise "Algorithm is of wrong length" unless algorithm.length == 512
 
 
 ## CALCULATE
+translation = {}
+translation.default_proc = ->(h,k){ raise "#{k.inspect} not in #{h.keys.inspect}" }
+translation['.'] = 0
+translation['#'] = 1
+
+inf_char = '.'
+
+inf_flip = {}
+inf_flip[0] = algorithm[ 0]
+inf_flip[1] = algorithm[-1]
+
 output_image = input_image
-filler_char = algorithm[0]
 
 2.times do |step|
-  puts"### #{step} ###"
+  #puts"### #{step} ###" #DEBUG
 
-  work_image = output_image.map{|line| [filler_char]+line+[filler_char] }
-  work_image.unshift Array.new(work_image[0].length, filler_char)
-  work_image.push    Array.new(work_image[0].length, filler_char)
-  work_image.each{|line| puts line.join };puts #DEBUG
+  work_image = output_image.map{|line| [inf_char]+line+[inf_char] }
+  work_image.unshift Array.new(work_image[0].length, inf_char)
+  work_image.push    Array.new(work_image[0].length, inf_char)
+  #work_image.each{|line| puts line.join };puts #DEBUG
 
-  output_image = work_image.map{|line| Array.new(line.length, filler_char) }
-  output_image.each{|line| puts line.join };puts #DEBUG
+  output_image = work_image.map{|line| Array.new(line.length, '_') }
+  #output_image.each{|line| puts line.join };puts #DEBUG
 
   m=work_image.length-1
   work_image.each_with_index do |line, i|
     w=line.length-1
     line.each_index do |j|
       index = [[],[],[]]
-      index[0][0] = (i==0||j==0) ? (filler_char) : (work_image[i-1][j-1])
-      index[0][1] = (i==0      ) ? (filler_char) : (work_image[i-1][j  ])
-      index[0][2] = (i==0||j==w) ? (filler_char) : (work_image[i-1][j+1])
-      index[1][0] = (      j==0) ? (filler_char) : (work_image[i  ][j-1])
+      index[0][0] = (i==0||j==0) ? (inf_char) : (work_image[i-1][j-1])
+      index[0][1] = (i==0      ) ? (inf_char) : (work_image[i-1][j  ])
+      index[0][2] = (i==0||j==w) ? (inf_char) : (work_image[i-1][j+1])
+      index[1][0] = (      j==0) ? (inf_char) : (work_image[i  ][j-1])
       index[1][1] =                                (work_image[i  ][j  ])
-      index[1][2] = (      j==w) ? (filler_char) : (work_image[i  ][j+1])
-      index[2][0] = (i==m||j==0) ? (filler_char) : (work_image[i+1][j-1])
-      index[2][1] = (i==m      ) ? (filler_char) : (work_image[i+1][j  ])
-      index[2][2] = (i==m||j==w) ? (filler_char) : (work_image[i+1][j+1])
+      index[1][2] = (      j==w) ? (inf_char) : (work_image[i  ][j+1])
+      index[2][0] = (i==m||j==0) ? (inf_char) : (work_image[i+1][j-1])
+      index[2][1] = (i==m      ) ? (inf_char) : (work_image[i+1][j  ])
+      index[2][2] = (i==m||j==w) ? (inf_char) : (work_image[i+1][j+1])
 
-      index = index.flatten.map{|char| char==filler_char ? 0 : 1 }.join.to_i(2)
+      index = index.flatten.map{|char| translation[char] }.join.to_i(2)
       output_image[i][j] = algorithm[index]
+      #output_image.each{|line| puts line.join };$stdin.gets("\n") #DEBUG
     end
   end
 
-  filler_char = (filler_char==".") ? algorithm[0] : algorithm[-1]
+  inf_char = inf_flip[ translation[inf_char] ]
 
-  output_image.each{|line| puts line.join };puts #DEBUG
+  #output_image.each{|line| puts line.join };puts #DEBUG
 end
 
 
