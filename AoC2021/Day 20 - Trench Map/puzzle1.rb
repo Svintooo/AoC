b1a07c0f6fcec29cc813c5e86bca4de74acd8424
Svintooo <github.com@svintoo.com>
2021-12-22 11:01:54 +0100

    AoC2021 day20: Initial commit
    Change-Id: Idd4dabcdf3aa80132e4d4b2fc5da98469156eaf1

---
+++ AoC2021/Day 20 - Trench Map/puzzle1.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+NEWLINE = /(?:\r\n?|\n)/
+
+
+## INPUT
+data = ARGF.read
+
+
+## PARSING
+asdf = data.lines
+#pp asdf #DEBUG
+#exit #DEBUG EXIT
+
+
+## CHECK
+#exit #DEBUG EXIT
+
+
+## HELP CODE
+#exit #DEBUG EXIT
+
+
+## CALCULATE
+
+
+## ANSWER
+answer = nil
+puts answer
