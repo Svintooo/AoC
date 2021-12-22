5ba7da7781da50e10eac01537f7788028d82234a
Svintooo <github.com@svintoo.com>
2021-12-22 14:58:56 +0100

    AoC2021 day20: Puzzle 1, ...
    Change-Id: Icf81cac976ea7551645443eff6296cfb67c1e29d

--- AoC2021/Day 20 - Trench Map/puzzle1.rb
+++ AoC2021/Day 20 - Trench Map/puzzle1.rb
@@ -14,6 +14,8 @@ algorithm, input_image = data.split(/#{NEWLINE}#{NEWLINE}/)
 
 algorithm = algorithm.strip.chars
 
+DEFAULT = algorithm[0]
+
 input_image = input_image.lines.map{|line| line.strip.chars }
 input_image.each{|line| puts line.join };puts #DEBUG
 #exit #DEBUG EXIT
@@ -33,12 +35,12 @@ output_image = input_image
 2.times do |step|
   #puts"### #{step} ###"
 
-  work_image = output_image.map{|line| ["."]+line+["."] }
-  work_image.unshift Array.new(work_image[0].length, ".")
-  work_image.push    Array.new(work_image[0].length, ".")
+  work_image = output_image.map{|line| [DEFAULT]+line+[DEFAULT] }
+  work_image.unshift Array.new(work_image[0].length, DEFAULT)
+  work_image.push    Array.new(work_image[0].length, DEFAULT)
   #work_image.each{|line| puts line.join };puts #DEBUG
 
-  output_image = work_image.map{|line| Array.new(line.length, ".") }
+  output_image = work_image.map{|line| Array.new(line.length, DEFAULT) }
   #output_image.each{|line| puts line.join };puts #DEBUG
 
   m=work_image.length-1
@@ -46,17 +48,17 @@ output_image = input_image
     w=line.length-1
     line.each_index do |j|
       index = [[],[],[]]
-      index[0][0] = (i==0||j==0) ? (".") : (work_image[i-1][j-1])
-      index[0][1] = (i==0      ) ? (".") : (work_image[i-1][j  ])
-      index[0][2] = (i==0||j==w) ? (".") : (work_image[i-1][j+1])
-      index[1][0] = (      j==0) ? (".") : (work_image[i  ][j-1])
+      index[0][0] = (i==0||j==0) ? (DEFAULT) : (work_image[i-1][j-1])
+      index[0][1] = (i==0      ) ? (DEFAULT) : (work_image[i-1][j  ])
+      index[0][2] = (i==0||j==w) ? (DEFAULT) : (work_image[i-1][j+1])
+      index[1][0] = (      j==0) ? (DEFAULT) : (work_image[i  ][j-1])
       index[1][1] =                        (work_image[i  ][j  ])
-      index[1][2] = (      j==w) ? (".") : (work_image[i  ][j+1])
-      index[2][0] = (i==m||j==0) ? (".") : (work_image[i+1][j-1])
-      index[2][1] = (i==m      ) ? (".") : (work_image[i+1][j  ])
-      index[2][2] = (i==m||j==w) ? (".") : (work_image[i+1][j+1])
+      index[1][2] = (      j==w) ? (DEFAULT) : (work_image[i  ][j+1])
+      index[2][0] = (i==m||j==0) ? (DEFAULT) : (work_image[i+1][j-1])
+      index[2][1] = (i==m      ) ? (DEFAULT) : (work_image[i+1][j  ])
+      index[2][2] = (i==m||j==w) ? (DEFAULT) : (work_image[i+1][j+1])
 
-      index = index.flatten.map{|char| char=="." ? 0 : 1 }.join.to_i(2)
+      index = index.flatten.map{|char| char==DEFAULT ? 0 : 1 }.join.to_i(2)
       output_image[i][j] = algorithm[index]
     end
   end
