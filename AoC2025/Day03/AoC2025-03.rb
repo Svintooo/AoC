9cc869fa1c83cef6d787c663b7af6fe869e5a5c5
Svintooo <github.com@svintoo.com>
2025-12-03 09:03:45 +0100

    AoC2025 Day03: Part 2 (improved)

--- AoC2025/Day03/AoC2025-03.rb
+++ AoC2025/Day03/AoC2025-03.rb
@@ -10,19 +10,16 @@ end
   .split(/\r?\n|\r/)
   .map{|line| line.chars }
   .map{|bank|
-    battery1, i = bank[0..-2].each_with_index.max_by{|jolt,| jolt }
-    battery2 = bank[(i+1)..-1].max
-    jolt1 = "#{battery1}#{battery2}".to_i
-    #
-    batteries = []
-    12.downto(1).each do |num|
-      battery, i = bank[0..-num].each_with_index.max_by{|jolt,| jolt }
-      batteries << battery
-      bank = bank[(i+1)..-1]
-    end
-    jolt2 = batteries.join("").to_i
-    #
-    [jolt1, jolt2]
+    fun = ->(batteries, battery_count_needed) {
+      selected_batteries = []
+      battery_count_needed.downto(1).each do |num|
+        battery, i = batteries[0..-num].each_with_index.max_by{|jolt,| jolt }
+        selected_batteries << battery
+        batteries = batteries[(i+1)..-1]
+      end
+      selected_batteries.join("").to_i
+    }
+    [fun.call(bank,2), fun.call(bank,12)]
   }
   .inject([0,0]){|(solution1, solution2), (jolt1, jolt2)| [solution1+jolt1, solution2+jolt2] }
   .tap{|result| puts result.inspect }
