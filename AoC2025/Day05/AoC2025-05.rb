5ca93c0410cdd8f570942320e7bad9aa3694ee4e
Svintooo <github.com@svintoo.com>
2025-12-05 23:05:57 +0100

    AoC2025 Day05: Part 2 (nicer)

--- AoC2025/Day05/AoC2025-05.rb
+++ AoC2025/Day05/AoC2025-05.rb
@@ -2,40 +2,44 @@
 newline = /(?:\r?\n|\r)/.source
 
 if !ARGV.empty?
-  IO.read(ARGV[0])
+    IO.read(ARGV[0])
 elsif !STDIN.tty?
-  STDIN.read
+     STDIN.read
 else
-  ["input", "input.txt", "input.example.txt"]
-    .find{|file| File.readable?(file) }  # Select first match
-    .yield_self{|file| IO.read(file) }
+    ["input", "input.txt", "input.example.txt"]
+        .find{|file| File.readable?(file) }  # Select first match
+        .yield_self{|file| IO.read(file) }
 end
-  .yield_self{|input| input.strip.split(/#{newline}{2,}/, 2) }
-  #.tap{|o| puts "[D] #{o.inspect}" }  # DEBUG
-  .tap{|fresh, avail| raise "Invalid fresh: #{fresh}" unless fresh =~ /^(\d+-\d+)(#{newline}\d+-\d+)+$/ }
-  .tap{|fresh, avail| raise "Invalid avail: #{avail}" unless avail =~ /^(\d+)(#{newline}\d+)+$/ }
-  .yield_self{|array| array.map{|str| str.split(/#{newline}/) } }
-  .yield_self{|fresh, avail| [fresh.map{|str| str.split("-",2).map(&:to_i).yield_self{|pair| Range.new(*pair)} }, avail] }
-  .yield_self{|fresh, avail| [fresh, avail.map{|str| str.to_i }] }
-  .yield_self{|fresh, avail|
-    part1 = avail.select{|ingr| fresh.any?{|range| range.cover?(ingr)} }.count
-
-    go_on = true
-    while go_on
-      go_on = false
-      fresh = fresh.inject([]) do |arr, r1|
-        r2, index = arr.each_with_index.find{|r,i| r.end >= r1.begin && r1.end >= r.begin }
-        if index
-          go_on = true
-          arr[index] = [r1.begin, r2.begin].min .. [r1.end, r2.end].max
-        else
-          arr << r1
-        end
-        arr
-      end
-    end
-    part2 = fresh.map{|range| range.count }.sum
-
-    [part1, part2]
-  }
-  .tap{|answers| p answers }
+    .yield_self{|input| input.strip.split(/#{newline}{2,}/, 2) }
+    #.tap{|o| puts "[D] #{o.inspect}" }  # DEBUG
+    .tap{|fresh, avail| raise "Invalid fresh: #{fresh}" unless fresh =~ /^(\d+-\d+)(#{newline}\d+-\d+)+$/ }
+    .tap{|fresh, avail| raise "Invalid avail: #{avail}" unless avail =~ /^(\d+)(#{newline}\d+)+$/ }
+    .yield_self{|array| array.map{|str| str.split(/#{newline}/) } }
+    .yield_self{|fresh, avail|
+        [
+            fresh.map{|str| str.split("-",2).map(&:to_i).yield_self{|pair| Range.new(*pair)} },
+            avail.map{|str| str.to_i }
+        ]
+    }
+    .yield_self{|fresh, avail|
+        part1 = avail.select{|ingr| fresh.any?{|range| range.cover?(ingr)} }.count
+        part2 =
+            fresh.inject([]){|arr, r1|
+                loop do
+                    loop_again = false
+                    arr.each{|r2|
+                        if r2.end >= r1.begin && r1.end >= r2.begin && !(r1.begin <= r2.begin && r1.end >= r2.end)
+                            loop_again = true
+                            r1 = [r1.begin, r2.begin].min .. [r1.end, r2.end].max
+                        end
+                    }
+                    break unless loop_again
+                end
+                arr.reject{|r2| r2.end >= r1.begin && r1.end >= r2.begin }
+                .push(r1)
+            }
+            .map{|range| range.count }
+            .sum
+        [part1, part2]
+    }
+    .tap{|answers| p answers }
