3ab765618aca37041d686969a3a4d776ae66173d
Svintooo <github.com@svintoo.com>
2025-12-06 10:39:52 +0100

    AoC2025 Day06: Part1

---
+++ AoC2025/Day06/AoC2025-06.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+newline = /(?:\r?\n|\r)/.source
+
+if !ARGV.empty?
+    IO.read(ARGV[0])
+elsif !STDIN.tty?
+     STDIN.read
+else
+    ["input", "input.txt", "input.example.txt"]
+        .find{|file| File.readable?(file) }  # Select first match
+        .yield_self{|file| IO.read(file) }
+end
+    .yield_self{|input| input.split(/#{newline}+/) }
+    .yield_self{|lines| lines.map(&:strip) }
+    .yield_self{|lines| lines.map{|line| line.split(/\s+/) } }
+    .yield_self{|rows|
+        columns = []
+        rows.each_with_index{|row, y|
+            row.each_with_index{|str, x|
+                columns[x] ||= []
+                columns[x][y] = str
+            }
+        }
+        columns
+    }
+    .map{|column| [ column[0..-2].map(&:to_i), column[-1].to_sym ] }
+    #.tap{|o| puts "[D] #{o.inspect}" }  #DEBUG
+    .map{|numbers, operator| numbers.inject(operator) }
+    .sum
+    .tap{|answer| p(answer) }
