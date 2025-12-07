2963f1cc87c39372ad0777e82d2e510ba28ce651
Svintooo <github.com@svintoo.com>
2025-12-07 18:19:00 +0100

    AoC2025 Day07: Part2

--- AoC2025/Day07/AoC2025-07.rb
+++ AoC2025/Day07/AoC2025-07.rb
@@ -12,9 +12,10 @@ end
 .yield_self do |input|
     splits = 0
     width = input.each_line.first.length
+    start_pos = input.index('S')
 
     visited = Set.new()
-    queue = [ input.index('S') + width ]
+    queue = [ start_pos + width ]
     while beam = queue.pop
         next if beam < 0 || beam >= input.length
         while input[beam] == '.' && !visited.include?(beam)
@@ -27,5 +28,33 @@ end
         end
     end
 
-    p splits
+
+    timelines = Hash.new(0)  # NOTE: Default value for new keys will be zero
+
+    traverse_method = :pop  # :pop, :shift
+    queue = [ [start_pos + width, [start_pos]] ]
+    while !queue.empty?
+        beam_start, history = queue.send(traverse_method)
+        beam = beam_start
+        next if beam < 0 || beam >= input.length
+        loop do
+            case input[beam]
+            when '^'
+                if timelines.has_key?(beam)
+                    history.each{|pos| timelines[pos] += timelines[beam] }
+                else
+                    queue << [beam-1, history + [beam]]
+                    queue << [beam+1, history + [beam]]
+                end
+                break
+            when nil  # Out of bounds
+                history.each{|pos| timelines[pos] += 1 }
+                break
+            else
+                beam += width
+            end
+        end
+    end
+
+    p [ splits, timelines[start_pos] ]
 end
